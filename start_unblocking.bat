0d604935f0b1c4449a69cc5e2cba72206b2a0026
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-19 22:51:16 -0400

    Update start_unblocking.bat

--- start_unblocking.bat
+++ start_unblocking.bat
@@ -1,4 +1,19 @@
 @echo off
+:: Auto-updater for launcher
+set "scriptUrl=https://raw.githubusercontent.com/DoubleAAGuy/myscripts/refs/heads/main/start_unblocking.bat"
+set "tempScript=%%TEMP%%\start_unblocking_new.bat"
+curl -s -L "%%scriptUrl%%" -o "%%tempScript%%"
+if exist "%%tempScript%%" ^(
+    fc "%%~f0" "%%tempScript%%" ^>nul 2^>^&1
+    if !errorlevel! neq 0 ^(
+        echo Updating launcher...
+        move "%%tempScript%%" "%%~f0" ^>nul
+        start "" "%%~f0"
+        exit
+    ^) else ^(
+        del "%%tempScript%%"
+    ^)
+^)
 echo Starting bypass...
 :: -D 1080 sets up the SOCKS proxy
 :: -pw password67 handles the authentication
