7a6a10ad9998e858fe0d126a673673464a5427d0
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-20 08:41:49 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -56,21 +56,6 @@ echo Creating Launcher with Auto-Update...
 (
 echo @echo off
 echo setlocal enabledelayedexpansion
-echo :: Auto-updater for launcher
-echo set "scriptUrl=https://raw.githubusercontent.com/DoubleAAGuy/myscripts/refs/heads/main/start_unblocking.bat"
-echo set "tempScript=%%TEMP%%\start_unblocking_new.bat"
-echo curl -s -L "%%scriptUrl%%" -o "%%tempScript%%"
-echo if exist "%%tempScript%%" ^(
-echo     for %%%%I in ^("%%tempScript%%"^ text^) do if %%%%~zI GTR 0 ^(
-echo         fc "%%~f0" "%%tempScript%%" ^>nul 2^>^&1
-echo         if ^!errorlevel^! neq 0 ^(
-echo             echo Updating launcher...
-echo             move /y "%%tempScript%%" "%%~f0" ^>nul ^& start "" "%%~f0" ^& exit
-echo         ^) else ^(
-echo             del "%%tempScript%%"
-echo         ^)
-echo     ^)
-echo ^)
 echo echo Starting bypass...
 echo start "" /b "%plinkDest%" -batch -pw password67 -hostkey "SHA256:91kWQJz3BT8C9UhfjzeoIPp28Ak7wNODxypuaCyeAxU" -N -D 1080 ezbat@doubleaaguy.duckdns.org
 echo echo Launching Brave...
