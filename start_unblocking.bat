32eb544df0bf6a0ee3438d05c7ad7bd92cdd9285
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-19 22:47:47 -0400

    Create start_unblocking.bat

---
+++ start_unblocking.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting bypass...
+:: -D 1080 sets up the SOCKS proxy
+:: -pw password67 handles the authentication
+:: -batch prevents interactive prompts
+:: -N ensures no shell is opened
+start "" /b "%%USERPROFILE%%\Desktop\BravePortable\plink.exe" -batch -pw password67 -N -D 1080 ezbat@doubleaaguy.duckdns.org
+echo Launching Brave...
+start "" "%exePath%"
+exit
