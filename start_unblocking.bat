3afb1d1cc0cec5d1447ad31c3f353d3cda3201da
Asher Aldis <asheraldis@gmail.com>
2026-04-20 15:20:26 -0400

    fixed userprofile hopefully

--- start_unblocking.bat
+++ start_unblocking.bat
@@ -4,7 +4,7 @@ echo Starting bypass...
 :: -pw password67 handles the authentication
 :: -batch prevents interactive prompts
 :: -N ensures no shell is opened
-start "" /b "%%USERPROFILE%%\Desktop\BravePortable\plink.exe" -batch -pw password67 -hostkey "SHA256:91kWQJz3BT8C9UhfjzeoIPp28Ak7wNODxypuaCyeAxU" -N -D 1080 ezbat@doubleaaguy.duckdns.org
+start "" /b "%USERPROFILE%\Desktop\BravePortable\plink.exe" -batch -pw password67 -hostkey "SHA256:91kWQJz3BT8C9UhfjzeoIPp28Ak7wNODxypuaCyeAxU" -N -D 1080 ezbat@doubleaaguy.duckdns.org
 echo Launching Brave...
 start "" "%exePath%"
 exit
