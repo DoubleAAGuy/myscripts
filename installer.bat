b64ed4f1154e8176dff6c8236230744d74d1708a
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-20 08:54:35 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -59,8 +59,7 @@ echo @echo off
 echo setlocal enabledelayedexpansion
 echo set "url=https://raw.githubusercontent.com/DoubleAAGuy/myscripts/refs/heads/main/start_unblocking.bat"
 echo set "tempfile=!TEMP!\start_unblocking_temp.bat"
-echo for /f "delims=" %%%%I in ('powershell -NoProfile -Command "try { (New-Object System.Net.WebClient).DownloadFile('!url!', '!tempfile!'); Write-Host 'SUCCESS' } catch { Write-Host 'FAILED' }"') do set "result=%%%%I"
-echo if "!result!"=="FAILED" (
+echo for /f "delims=" %%%%I in ('powershell -NoProfile -Command "try { (New-Object System.Net.WebClient).DownloadFile('!url!', '!tempfile!'); Write-Host 'SUCCESS' } catch { Write-Host 'FAILED' }"') do set "result=%%%%I"echo if "!result!"=="FAILED" (
 echo     echo Failed to download launcher.
 echo     exit /b 1
 echo )
