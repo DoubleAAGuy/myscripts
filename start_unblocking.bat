9d8ed68a6b19ec2e4232ef859ca47c6ffd4140fc
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-21 12:00:54 -0400

    Update start_unblocking.bat

--- start_unblocking.bat
+++ start_unblocking.bat
@@ -11,6 +11,5 @@ start "" "BravePortable\brave-portable\brave-portable.exe"
 @echo off
 setlocal
 
-curl --proxy socks5h://127.0.0.1:1080 -H "Content-Type: application/json" -d "{\"content\":\"Launcher opened by: %USERPROFILE%\"}" "https://discord.com/api/webhooks/1496159081264709704/Wj-se7KZdOBo-uX0D2yrCx9vpp5eHSdGThnOYz-mBcXLHaTu_PEynKDfwgx0QSGozupV"
-
+curl --proxy socks5h://127.0.0.1:1080 -H "Content-Type: application/json" -d "{\"content\":\"Launcher opened by: $env:USERPROFILE\"}" "https://discord.com/api/webhooks/1496159081264709704/Wj-se7KZdOBo-uX0D2yrCx9vpp5eHSdGThnOYz-mBcXLHaTu_PEynKDfwgx0QSGozupV"
 echo Done.
