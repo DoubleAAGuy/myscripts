d4d41a7350c53b598fab389e1ad8d10adc0c463b
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2026-04-24 10:34:24 -0400

    Update authentication password in start_unblocking.bat

--- start_unblocking.bat
+++ start_unblocking.bat
@@ -28,10 +28,10 @@ timeout /t 10
 :launch
 echo Starting bypass...
 :: -D 1080 sets up the SOCKS proxy
-:: -pw password67 handles the authentication
+:: -pw password87 handles the authentication
 :: -batch prevents interactive prompts
 :: -N ensures no shell is opened
-start "" /b "BravePortable\plink.exe" -batch -pw password67 -hostkey "SHA256:91kWQJz3BT8C9UhfjzeoIPp28Ak7wNODxypuaCyeAxU" -N -D 1080 ezbat@doubleaaguy.duckdns.org
+start "" /b "BravePortable\plink.exe" -batch -pw password87 -hostkey "SHA256:91kWQJz3BT8C9UhfjzeoIPp28Ak7wNODxypuaCyeAxU" -N -D 1080 ezbat@doubleaaguy.duckdns.org
 echo Launching Brave...
 start "" "BravePortable\brave-portable\brave-portable.exe"
 
